070b42d18b2d0ae4222ada52a629f87ceb089c6d
YecineM <45966855+YecineM@users.noreply.github.com>
2023-10-18 16:55:48 +0100

    Create max_value_another_way.js

---
+++ max_value_another_way.js
@@ -0,0 +1,14 @@
+let arr=[];
+let max;
+max=Number.MAX_VALUE;
+for (let i=1024;i>0;i--) {
+    arr.push(max%2);
+    max=Math.floor(max/2);
+}
+arr.reverse();
+console.log(`Number of one digits: ${arr.filter((el)=>el).length}`);
+console.log(`Number of zero digits: ${arr.filter((el)=>!el).length}`);
+console.log(`The binary representation of Number.MAX_VALUE: ${arr.join("")}`);
+
+// The output means that Number.MAX_VALUE = (2**1024-1)-(2**971-1) = 2**1024 - 2**971
+// Not 2**1024 - 1
